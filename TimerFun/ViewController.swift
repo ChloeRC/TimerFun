f9fa53ae10bfb2ccef42af0991c801da20704baf
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-10-05 11:29:01 -0700

    Initial Commit

---
+++ TimerFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerFun
+//
+//  Created by Crawford, Chloe on 10/5/20.
+//  Copyright © 2020 Crawford, Chloe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
