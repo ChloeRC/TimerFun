bb5c42113904a2cc2518f6160e0b4cc66809b1fc
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-10-05 11:46:50 -0700

    class end

--- TimerFun/ViewController.swift
+++ TimerFun/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // task: define seconds property (Int)
+    // prop observer updates secondsLabel
+    
+    //task: when press pause or reset, invalidate the timer
+    
+    
+    //var seconds: Int = {
+        // have it update seconds in label
+        //return 0
+    //}
 
+    var timer: Timer? = nil
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            print("tick")
+            //seconds += 1
+        })
+    }
+    
+    @IBOutlet weak var secondsLabel: UILabel!
+    
+    @IBAction func startButton() {
+        print("start")
+        startTimer()
+    }
+    
+    @IBAction func pauseButton() {
+        print("pause")
+    }
+    
+    @IBAction func resetButton() {
+        print("reset")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
